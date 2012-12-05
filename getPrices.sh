5ce5b0b98e7be390686264a616016aa40ba77c37
Daniel Correa Lobato <dclobato@users.noreply.github.com>
2012-12-05 23:35:49 +0000

    Inclusao de suporte a Tesouro Direto

--- getPrices.sh
+++ getPrices.sh
@@ -15,6 +15,10 @@ TR=`which tr`
 CUT=`which cut`
 SORT=`which sort`
 READ=`which read`
+XLS2CSV=`which xls2csv`
+TAIL=`which tail`
+
+WGETOPT="--no-check-certificate -q --user-agent=\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2\""
 
 ## BBURL="http://www21.bb.com.br/portalbb/cotaFundos/GFI9,2,null,null,006.bbx?tipo=5\&fundo="
 BBURL="http://www37.bb.com.br/portalbb/cotaFundos/GFI9,2,001.bbx?tipo=5\&fundo="
@@ -22,6 +26,7 @@ GFURL="https://online.gerafuturo.com.br/onlineGeracao/PortalManager?show=produto
 BCURL="http://www4.bcb.gov.br/pec/taxas/port/PtaxRPesq.asp"
 BVURL="http://www.infomoney.com.br/Pages/Download/Download.aspx?dtIni=null\&dtFinish=null\&Semana=null\&Per=3\&type=1\&StockType=1\&Stock="
 BVURL2="\&Ativo="
+TDURL="https://www.tesouro.fazenda.gov.br/images/arquivos/artigos/"
 
 #### Remove linhas em branco
 BASEPARSER="$AWK '/./' | $SED -e 's/^ *//'"
@@ -34,14 +39,19 @@ BBPARSE="$SED '1,3d' | $SED -e :a -e '\$d;N;2,3ba' -e 'P;D' | $TR -s ' ' ';' | $
 GFPARSE="$TR -s ' ' ';' | $TR -s ',' '.' | $AWK -F '[/|;]' '{ printf \"%s%s%s %s\n\", \$3, \$2, \$1, \$4 ; }'"
 BCPARSE="$CUT -s -f 1,6 -d ';' | $AWK -F ';' '{ printf \"%s %f\\n\", \$1, \$2 ; }' | $TR ',' '.' | $AWK -F '\\0' '{ print substr(\$0, 5, 4) substr(\$0, 3, 2) substr(\$0, 1, 2), substr(\$0, 10)}'"
 BVPARSE="$SED -E -e 's/^ *//;1d;s/ +/ /g;s/\.//g' | $CUT -s -f 1,2,6,8,9 -d ' ' | $TR -s ' ' ';' | $TR -s ',' '.' | $AWK -F '[/|;]' '{ printf \"%s%s%s %s %s %s %s\\n\", \$3, \$2, \$1, \$4, \$5, \$6, \$7}'"
+TDPARSE="$SED '1,2d' | $CUT -f 1,5 -d ';' | $SED -E -e 's/,//g' | awk -F '[/|;]' '{printf \"%s%s%s %s\\n\", \$3, \$2, \$1, \$4}'"
 
 processLine(){
   line="$@" # get all args
 
-  TIPO=$(echo $line | awk '{ print $1 }')
-  SAIDA=$(echo $line | awk '{ print $2 }')
-  BANCO=$(echo $line | awk '{ print $3 }')
-  CODFU=$(echo $line | awk '{ print $4 }')
+  TIPO=$(echo $line | awk '{ print $1 }')    # Tipo de ativo
+  SAIDA=$(echo $line | awk '{ print $2 }')   # Nome arquivo de saida (= Security ID/Ticker Symbol)
+  BANCO=$(echo $line | awk '{ print $3 }')   # Fonte dos dados
+  CODFU=$(echo $line | awk '{ print $4 }')   # Codigo do ativo
+  FILTD=$(echo $line | awk '{ print $5 }')   # Nome do arquivo cotacoes (apenas para TIPO="TD")
+  VENTD=$(echo $line | awk '{ print $6 }')   # Vencimento do titulo publico (apenas para TIPO="TD")
+  PRETD=$(echo $line | awk '{ print $7 }')    # Prefixo na planilha (apenas para TIPO="TD")
+  
 
   tmpFile1="$DIRETORIO$SAIDA.1.tmp"
   tmpFile2="$DIRETORIO$SAIDA.2.tmp"
@@ -68,7 +78,6 @@ processLine(){
      $WGET -q "$BCURL$BCPAR" -O $tmpFile1
      eval "$ELINKS $ELINKSPAR $tmpFile1 > $tmpFile2"
      TOGET=`cat $tmpFile2 | grep "download/cotacoes/BC" | cut -f 3 -d " " | uniq`
-     #echo $TOGET
      $WGET $TOGET -q -O $tmpFile2
      cat $tmpFile2 | eval "$BASEPARSER | $BCPARSE" > $tmpFile1
      eval "$ENDPARSER < $tmpFile1 >$finalFile"
@@ -77,6 +86,16 @@ processLine(){
      eval "$ELINKS $ELINKSPAR $BVURL$CODFU$BVURL2$CODFU > $tmpFile1"
      cat $tmpFile1 | eval "$BASEPARSER | $BVPARSE > $tmpFile2"
      eval "$ENDPARSER < $tmpFile2 >$finalFile"
+  fi
+  if [ "$BANCO" == "TD" ]; then
+     eval "$WGET $WGETOPT $TDURL$FILTD.xls"
+     if [ "$PRETD" = "NTN-B-P" ]; then
+        PRETD="NTN-B Principal"
+     fi
+     eval "$XLS2CSV -q -x $FILTD.xls -w \"$PRETD $VENTD\" -c $tmpFile1"
+     cat $tmpFile1 | eval "$BASEPARSER | $TDPARSE | $TAIL -n 120 > $tmpFile2"
+     eval "$ENDPARSER < $tmpFile2 >$finalFile"
+     rm $FILTD.xls
   fi
 
   echo -n "  Cotacao mais atual >> "
